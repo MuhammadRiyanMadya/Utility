0cc961b73a67f00bd7ba55e80a03873ef3c476cd
M.M.R <140324657+MuhammadRiyanMadya@users.noreply.github.com>
2023-09-17 23:32:28 +0700

    Add movement algorithm tictac.py

--- src/tictac.py
+++ src/tictac.py
@@ -5,18 +5,37 @@ Created on Sat Sep 16 18:52:34 2023
 @author: MMR
 """
 
-def display_board():
+def display_board(df):
+    # df is a list showing the box status
+    # df depends on user and computer input to the list
     print('+'+'-'*7+'+'+'-'*7+'+'+'-'*7+'+')
     print(('|'+' '*7)*3 +'|')
-    print('|'+' '*3 + '1' + ' '*3 +'|'+' '*3 + '2' + ' '*3+'|'+' '*3 + '3' + ' '*3 +'|')
+    print('|'+' '*3 + f'{df[0]}' + ' '*3 +'|'+' '*3 + f'{df[1]}' + ' '*3+'|'+' '*3 + f'{df[2]}' + ' '*3 +'|')
     print(('|'+' '*7)*3 +'|')
     print('+'+'-'*7+'+'+'-'*7+'+'+'-'*7+'+')
     print(('|'+' '*7)*3 +'|')
-    print('|'+' '*3 + '1' + ' '*3 +'|'+' '*3 + '2' + ' '*3+'|'+' '*3 + '3' + ' '*3 +'|')
+    print('|'+' '*3 + f'{df[3]}' + ' '*3 +'|'+' '*3 + f'{df[4]}' + ' '*3+'|'+' '*3 + f'{df[5]}' + ' '*3 +'|')
     print(('|'+' '*7)*3 +'|')
     print('+'+'-'*7+'+'+'-'*7+'+'+'-'*7+'+')
     print(('|'+' '*7)*3 +'|')
-    print('|'+' '*3 + '1' + ' '*3 +'|'+' '*3 + '2' + ' '*3+'|'+' '*3 + '3' + ' '*3 +'|')
+    print('|'+' '*3 + f'{df[6]}' + ' '*3 +'|'+' '*3 + f'{df[7]}' + ' '*3+'|'+' '*3 + f'{df[8]}' + ' '*3 +'|')
     print(('|'+' '*7)*3 +'|')
     print('+'+'-'*7+'+'+'-'*7+'+'+'-'*7+'+',end='')
-display_board()
+def move()
+    df = [1,2,3,4,5,6,7,8,9]
+    start_display = display_board(df)
+    while True:
+        # computer input
+        While True:
+            com_in = randrange(9)
+            for i in df:
+                if com_in == i:
+                    df[i-1] = com_in
+                    break
+        update_display = display_board(df)
+        # user input
+        user_in = int(input("Input your move: ")
+        for i in df:
+            if user_in == i:
+                df[i-1] = com_in
+        update_display = display_board(df)
