957819aa2d46199587f0d06fff695bc4447e015a
M.M.R <140324657+MuhammadRiyanMadya@users.noreply.github.com>
2024-11-19 10:59:23 +0700

    Update plotify.py

--- src/plotify.py
+++ src/plotify.py
@@ -1,14 +1,15 @@
-# Create dummy data indexed by month and with multi-columns [product, revenue]
+#!/usr/bin/env python
 import matplotlib.pyplot as plt
 import numpy as np
 from datetime import datetime
 from matplotlib.patches import Rectangle
 
 
-def plotify(stats:dict, imgTitle):
+def plotify(stats:dict, imgTitle, latestVal):
     
-    locs = ('Bulk', 'Utility', 'Pelletizing 1', 'Pelletizing 2', 'BM 1', 'BM 2')
+    locs = ('Bulk', 'Utility', 'PCR 1', 'PCR 2', 'BM 1', 'BM 2')
     cluster = {
+        # test
 ##        'Process': np.array([7, 3, 6,2,3,4]),
 ##        'Quality': np.array([3, 4, 8,1,3, 5]),
 ##        'Safety': np.array([7, 3, 5,1,3, 5]),
@@ -16,35 +17,36 @@ def plotify(stats:dict, imgTitle):
         'Quality': np.array(stats['Quality']),
         'Safety': np.array(stats['Safety']),
     }
-    width = 0.4  # the width of the bars: can also be len(x) sequence
+    width = 0.4
 
     color = ["#0288D1", "#CE93D8", "#EF9A9A"]
     labels = ['Safety', 'Quality', 'Process']
     fig, ax = plt.subplots()
     bottom = np.zeros(6)
     i = 0
-    nLabel = 1
+
     for cluster, clusterNumber in cluster.items():
         p = ax.bar(locs, clusterNumber, width, label=cluster, bottom=bottom, color = color[i])
         bottom += clusterNumber
         i += 1
         
+##        ax.bar_label(p, label_type='center')
 
-    ax.set_title('+1' + 'Bulk: )
+    ax.set_title(r"$\bf{" + '+1' + "}$ at " + latestVal)
     ax.legend()
     
-##    fig.savefig(r'C:\Users\mrm\Documents\Eng\Reporting system\__buffer' + '\\' + imgTitle)
-    
+    fig.savefig(r'C:\Users\ssv\AppData\Local\Programs\Python\Python311\InjectionX\__program\__buffer' + '\\' + imgTitle)
     for p in ax.get_children()[:-1]:
         if isinstance(p, Rectangle):
             x, y = p.get_xy()
             w, h = p.get_width(), p.get_height()
             if h > 0:
                 ax.text(x + 0.5*w, y + 0.5*h, '%0.0f'%h, va='center', ha='center')
-        
-    plt.show()
+
+    
+##    plt.show()
     return
 
 # test
 
-plotify({'Process':[7,1,0,0,0,0], 'Quality':[3, 0,0,0,0,0], 'Safety':[0,0,0,0,0,1]}, 'alfa.png')
+##plotify({'Process':[7,3,0,0,0,0], 'Quality':[7,3,6,2,3,4], 'Safety':[7,3,6,2,3,4]}, '12-12-13.png', 'Bulk:Quality')
